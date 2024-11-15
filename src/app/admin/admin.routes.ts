4b2c131f58eb4fb3b601f627b919a0085b236268
Victor <vramirezgarcia64@correo.unicordoba.edu.co>
2024-11-15 10:48:23 -0500

    feat: add route for products component in admin module

--- src/app/admin/admin.routes.ts
+++ src/app/admin/admin.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from "@angular/router";
 import { AdminComponent } from "./admin.component";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
 import { PostCategoryComponent } from "./components/post-category/post-category.component";
+import { PostProductComponent } from "./components/post-product/post-product.component";
 
 export const ADMIN_ROUTES: Routes = [
     {path: '', component: AdminComponent},
     {path: 'dashboard', component: DashboardComponent},
-    {path: 'category', component: PostCategoryComponent}
+    {path: 'category', component: PostCategoryComponent},
+    {path: "products", component: PostProductComponent}
 ]
