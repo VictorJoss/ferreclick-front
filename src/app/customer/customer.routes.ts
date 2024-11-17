1dba25d8c7d200cfe4b5b143d88ccbabcb493525
Victor <vramirezgarcia64@correo.unicordoba.edu.co>
2024-11-17 16:07:30 -0500

    feat: add route for product details in dashboard

--- src/app/customer/customer.routes.ts
+++ src/app/customer/customer.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from "@angular/router";
 import { CustomerComponent } from "./customer.component";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { ProductDetailsComponent } from "./components/product-details/product-details.component";
 
 export const CUSTOMER_ROUTES: Routes = [
     {path: '', component: CustomerComponent},
-    {path: 'dashboard', component: DashboardComponent}
+    {path: 'dashboard', component: DashboardComponent},
+    { path: 'dashboard/product-details/:id', component: ProductDetailsComponent },
 ]
