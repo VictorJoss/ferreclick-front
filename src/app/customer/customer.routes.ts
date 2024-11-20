3447fbcaa4a4be4dd3a384ac6c25d792aae6d648
Victor <vramirezgarcia64@correo.unicordoba.edu.co>
2024-11-19 23:05:57 -0500

    feat: add customer guard to protect dashboard and product details routes

--- src/app/customer/customer.routes.ts
+++ src/app/customer/customer.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from "@angular/router";
 import { CustomerComponent } from "./customer.component";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
 import { ProductDetailsComponent } from "./components/product-details/product-details.component";
+import { CustomerGuard } from "../guards/customer/customer.guard";
 
 export const CUSTOMER_ROUTES: Routes = [
     {path: '', component: CustomerComponent},
-    {path: 'dashboard', component: DashboardComponent},
-    { path: 'dashboard/product-details/:id', component: ProductDetailsComponent },
+    {path: 'dashboard', component: DashboardComponent, canActivate: [CustomerGuard]},
+    { path: 'dashboard/product-details/:id', component: ProductDetailsComponent, canActivate: [CustomerGuard]},
 ]
