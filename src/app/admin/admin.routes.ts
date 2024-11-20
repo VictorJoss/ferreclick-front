abb72702f2b489c290b64b2b8e3f9093c3a660bc
Victor <vramirezgarcia64@correo.unicordoba.edu.co>
2024-11-19 23:05:34 -0500

    feat: add admin guard to protect admin routes

--- src/app/admin/admin.routes.ts
+++ src/app/admin/admin.routes.ts
@@ -3,10 +3,11 @@ import { AdminComponent } from "./admin.component";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
 import { PostCategoryComponent } from "./components/post-category/post-category.component";
 import { PostProductComponent } from "./components/post-product/post-product.component";
+import { AdminGuard } from "../guards/admin/admin.guard";
 
 export const ADMIN_ROUTES: Routes = [
     {path: '', component: AdminComponent},
-    {path: 'dashboard', component: DashboardComponent},
-    {path: 'category', component: PostCategoryComponent},
-    {path: "products", component: PostProductComponent}
+    {path: 'dashboard', component: DashboardComponent, canActivate: [AdminGuard]},
+    {path: 'category', component: PostCategoryComponent, canActivate: [AdminGuard]},
+    {path: "products", component: PostProductComponent, canActivate: [AdminGuard]}
 ]
