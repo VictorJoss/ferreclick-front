bdc4fded0a41bb389c5f40e288a767b4d17c4052
Victor <vramirezgarcia64@correo.unicordoba.edu.co>
2024-11-21 22:52:29 -0500

    feat: add new components to general layout

--- src/app/general/general.component.ts
+++ src/app/general/general.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { InfoTagHomeComponent } from '../customer/components/info-tag-home/info-tag-home.component';
+import { HeroComponent } from '../customer/components/hero/hero.component';
+import { HomeAdvantagesComponent } from '../customer/components/home-advantages/home-advantages.component';
+import { ProductsectionComponent } from '../customer/components/productsection/productsection.component';
+import { FooterComponent } from '../customer/components/footer/footer.component';
 
 @Component({
   selector: 'app-general',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet,NavbarComponent,InfoTagHomeComponent, HeroComponent, HomeAdvantagesComponent, ProductsectionComponent, FooterComponent],
   templateUrl: './general.component.html',
   styleUrl: './general.component.css'
 })
