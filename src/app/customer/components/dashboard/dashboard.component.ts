1d58d452f152999e8a94b4fac84b6cba45c169c0
Victor <vramirezgarcia64@correo.unicordoba.edu.co>
2024-12-06 13:29:55 -0500

    feat: add chat bot component and integrate into dashboard

--- src/app/customer/components/dashboard/dashboard.component.ts
+++ src/app/customer/components/dashboard/dashboard.component.ts
@@ -6,12 +6,13 @@ import { HeroComponent } from '../../../UtilitiesComponents/hero/hero.component'
 import { HomeAdvantagesComponent } from '../../../UtilitiesComponents/home-advantages/home-advantages.component';
 import { ProductsectionComponent } from '../productsection/productsection.component';
 import { FooterComponent } from '../footer/footer.component';
+import { ChatBotComponent } from "../chat-bot/chat-bot.component";
 
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,InfoTagHomeComponent, HeroComponent, HomeAdvantagesComponent, ProductsectionComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, InfoTagHomeComponent, HeroComponent, HomeAdvantagesComponent, ProductsectionComponent, FooterComponent, ChatBotComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
